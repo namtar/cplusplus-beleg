ec1caaf835e8c024904d61a067b548c615bace15
namtar <matthias.drummer@gmail.com>
2014-06-07 17:24:47 +0200

    basic implementation of add and remove

--- libapi/list_api.cpp
+++ libapi/list_api.cpp
@@ -1,26 +1,52 @@
 #include "list_api.h"
+#include <cstdlib>
 
 using namespace HTW::AI::Beleg;
+
+ListPerson* personList = NULL; // init with null. This pointer shall point to the root item of the list.
 
 /**
  * {@inheritDoc}
  */
 ListPerson* getList() {
-    // TODO: implement    
+    return personList;
 }
 
 /**
  * {@inheritDoc}
  */
 bool add(Person* data) {
-    // TODO: implement
+    // TODO: testme
+
+    ListPerson *actual = personList; // get first element
+    if (actual == NULL) {
+        // there is no lists. create one
+        actual = new ListPerson();
+        actual->data = data;
+    } else {
+        // search end of list
+        bool done = false;
+        while (!done) {
+
+            if (actual->next == NULL) {
+                // reached end of the list.
+                // append here
+                ListPerson* newListPersion = new ListPerson();
+                newListPersion->data = data;
+                actual->next = newListPersion;
+                done = true;
+            }
+        }
+    }
+
 }
 
 /**
  * {@inheritDoc}
  */
 bool insertAfter(ListPerson* element, Person* data) {
-    // TODO: implement
+
+
 }
 
 /**
@@ -34,5 +60,27 @@ bool insertBefore(ListPerson* element, Person* data) {
  * {@inheritDoc}
  */
 void remove(ListPerson* element) {
-    // TODO: implement
+    
+    // TODO: testme
+    ListPerson *actual = personList; // get first element
+
+    // get the right position of the element
+    // get also the elements position after the element which has to be removed
+    bool done = false;
+    while (!done) {
+
+        if (actual->next == element) {
+            ListPerson *before = actual;
+            // lets see if there is an following object
+            ListPerson *after = NULL;
+            if(actual->next->next != NULL) {
+                after = actual->next->next;
+            }
+            // so now do the removing
+            before->next = after;
+            // free memory of the removed element
+            delete element;
+        }
+    }
+    
 }
