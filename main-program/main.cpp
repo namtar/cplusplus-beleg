6c4256974cb438332d0e12774f70f20f4bff553e
namtar <matthias.drummer@gmail.com>
2014-06-08 22:40:56 +0200

    implemented the reading of a folder

--- main-program/main.cpp
+++ main-program/main.cpp
@@ -7,6 +7,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <dirent.h>
 #include "plugin.h"
 
 using namespace std;
@@ -15,6 +16,18 @@ using namespace std;
  * Prints the common menu which contains the basic functionality which is provided by the list api
  */
 void printCommonMenu();
+
+/**
+ * Gets the user input from the menue.
+ * 
+ * @return the chosen menu entry index
+ */
+int getMenuInput();
+
+/**
+ * Loads the modules from the pluginsfolder and adds the handles to a list.
+ */
+void loadModules();
 
 /*
  * @See: http://stackoverflow.com/questions/1142103/how-do-i-load-a-shared-object-in-c
@@ -27,7 +40,20 @@ void printCommonMenu();
  */
 int main(int argc, char** argv) {
 
-    printCommonMenu();
+    bool done = false;
+    
+    loadModules();
+
+    while (!done) {
+        printCommonMenu();
+        int input = getMenuInput();
+        switch (input) {
+            case 0:
+                done = true;
+                break;
+        }
+    }
+
     return 0;
 }
 
@@ -39,5 +65,35 @@ void printCommonMenu() {
     cout << "2: remove Element" << endl;
     cout << "3: insert Element before" << endl;
     cout << "4: insert Element after" << endl;
+    cout << "0: End program" << endl;
+}
+
+int getMenuInput() {
+
+    cout << "Choose: ";
+    int input = -1;
+    cin >> input;
+
+    // TODO: do some validation here. perhaps put it all in a loop until a valid menu entry has been chosen
+    return input;
+}
+
+void loadModules() {
+
+    DIR* dir = opendir("plugins");
+    dirent* pdir;
+
+    while (pdir = readdir(dir)) {
+        cout << pdir->d_name << endl; // working..... load only special files
+    }
+
+    closedir(dir);
+
+    // open plugins folder
+    // see what files are in it
+    // try to load so
+    // verify that it has the correct header
+
+    // store it in a structure so that the menu can get generated dynamically
 }
 
