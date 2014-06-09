a739f8de4ee9dc5bfc1c6512b91ffb390893d179
namtar <matthias.drummer@gmail.com>
2014-06-09 21:22:32 +0200

    name mangeling fecking problem

--- main-program/main.cpp
+++ main-program/main.cpp
@@ -10,11 +10,15 @@
 #include <dirent.h>
 #include "plugin.h"
 #include <dlfcn.h>
+#include <string>
+#include <list>
 
 using namespace std;
 using namespace HTW::AI::Beleg;
 
 typedef ListPerson* (*f_point)();
+
+list<void*> handles;
 
 /**
  * Prints the common menu which contains the basic functionality which is provided by the list api
@@ -46,26 +50,26 @@ void loadModules();
 int main(int argc, char** argv) {
 
     char* error;
-    
+
     // initial load liblibapi.so
     void* handle = dlopen("./liblibapi.so", RTLD_LAZY);
     // check for error opening the lib
     error = dlerror();
-    if(error) {
+    if (error) {
         cout << error << endl;
         return 1;
     }
 
-//    f_point getList = (f_point) dlsym(handle, "_Z7getListv");    
-    f_point getList = (f_point) dlsym(handle, "getList");    
+    //    f_point getList = (f_point) dlsym(handle, "_Z7getListv");    
+    f_point getList = (f_point) dlsym(handle, "_Z7getListv");
     error = dlerror();
-    if(error) {
+    if (error) {
         cout << error << endl;
         return 1;
     }
-    
+
     bool done = false;
-    
+
     loadModules();
 
     while (!done) {
@@ -81,8 +85,10 @@ int main(int argc, char** argv) {
                 break;
         }
     }
-    
+
     dlclose(handle);
+
+    cout << "handles size" << handles.size() << endl;
 
     return 0;
 }
@@ -112,18 +118,28 @@ void loadModules() {
 
     DIR* dir = opendir("plugins");
     dirent* pdir;
+    char* error;
 
     while (pdir = readdir(dir)) {
-        cout << pdir->d_name << endl; // working..... load only special files
+        //        cout << pdir->d_name << endl; // working..... load only special files
+        string fileName = pdir->d_name;
+        // analyze if the first three chars are lib
+        // see if the name contains .so
+        // if this is the case load lib
+        if ((fileName.compare(0, 2, "lib") == 0) && (fileName.find(".so", 0))) {
+
+            void* handle = dlopen(fileName.c_str(), RTLD_LAZY);
+            error = dlerror();
+            if (error) {
+                cout << "An error occured when opening a shared library: " << error << endl;
+                // print error and continue. If lib can not be loaded then it is gods will ;)
+                continue;
+            }
+
+            handles.push_back(handle);
+        }
     }
 
     closedir(dir);
-
-    // open plugins folder
-    // see what files are in it
-    // try to load so
-    // verify that it has the correct header
-
-    // store it in a structure so that the menu can get generated dynamically
 }
 
